14defebc063c995cea6997714a555160ca9502de
Abhishek Khatri <45210859+khatri7@users.noreply.github.com>
2023-10-06 14:37:08 -0400

    feat(about): image placeholder and design

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 import React, { cache } from "react";
 import styles from "./page.module.css";
 import { GET } from "@/utils/api-calls";
@@ -68,37 +68,37 @@ const About: React.FC = async () => {
 				<Container>
 					<Grid spacing={4} container>
 						{authors.map((author) => (
-							<Grid item xs={12} sm={6} md={4} lg={3} key={author.id}>
+							<Grid item xs={12} sm={6} md={4} lg={4} key={author.id}>
 								<Box>
-									<Box
-										sx={{
-											height: 370,
-											width: "100%",
-											mb: 2.5,
-											background: "#b3bec4",
-											overflow: "hidden",
-										}}
-										className={styles.authorProfile}
-									>
-										{author.avatar && (
+									<Stack direction="row" alignItems="center" gap={4}>
+										<Box
+											sx={{
+												width: 108,
+												height: 108,
+												overflow: "hidden",
+											}}
+											className={styles.authorProfile}
+										>
 											<img
-												src={author.avatar}
+												src={author.avatar || "/Bio-placeholder.png"}
 												alt={author.name}
 												className={styles.authorAvatar}
 											/>
-										)}
-									</Box>
-									<Typography
-										variant="h5"
-										fontSize={16}
-										lineHeight="24px"
-										fontWeight="medium"
-									>
-										{author.name}
-									</Typography>
-									<Typography variant="h5" lineHeight="24px" fontSize={16}>
-										{author.affiliation}
-									</Typography>
+										</Box>
+										<Box>
+											<Typography
+												variant="h5"
+												fontSize={16}
+												lineHeight="24px"
+												fontWeight="medium"
+											>
+												{author.name}
+											</Typography>
+											<Typography variant="h5" lineHeight="24px" fontSize={16}>
+												{author.affiliation}
+											</Typography>
+										</Box>
+									</Stack>
 								</Box>
 							</Grid>
 						))}
